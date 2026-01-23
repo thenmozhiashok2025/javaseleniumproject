f151a189d03ff47c961af4191b9dbbc4395d79ac
honey <honey@Thenmozhi.tx.rr.com>
2026-01-22 19:10:00 -0600

    changes in chromedriver location

--- src/javaPackage/FBLogin.java
+++ src/javaPackage/FBLogin.java
@@ -10,7 +10,7 @@ public class FBLogin {
 
 	public static void main(String[] args) {
 		// Launch of google chrome
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\honey\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver","C:\\Users\\honey\\Desktop\\selenium course\\chromedriver-win64\\chromedriver-win64.exe");
 
 WebDriver driver = new ChromeDriver();
 
@@ -18,9 +18,9 @@ driver.get("https://www.facebook.com");
 
 driver.manage().window().maximize();
 driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-//driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys("abc@gmail.com");
-//driver.findElement(By.xpath("//input[@name=\"pass\"]")).sendKeys("login");
-//driver.findElement(By.xpath("//button[@name='login']")).click();
+driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys("abc@gmail.com");
+driver.findElement(By.xpath("//input[@name=\"pass\"]")).sendKeys("login");
+driver.findElement(By.xpath("//button[@name='login']")).click();
 driver.findElement(By.xpath("//input[@id='reg_pages_msg']")).click();
 System.out.println("Success");
 	}
--- src/javaPackage/WaitsinSelenium.java
+++ src/javaPackage/WaitsinSelenium.java
@@ -1,5 +1,7 @@
 package javaPackage;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,13 +9,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WaitsinSelenium {
 
 	public static void main(String[] args) throws InterruptedException{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\honey\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\honey\\Desktop\\selenium course\\chromedriver-win64\\chromedriver-win64");
 
 		WebDriver driver = new ChromeDriver();
-
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.get("https://testautomationpractice.blogspot.com/");
 
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@id=\"name\"]")).sendKeys("abc@gmail.com");
+		System.out.println("Text entered successfully!");
+		
+	driver.quit();
 
 	}
 
